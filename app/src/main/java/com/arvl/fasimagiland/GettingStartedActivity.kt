9039421516277e2d5f51893fc2a5955e4211ae84
fahrulfirgiana <fahrulfirgiana10@gmail.com>
2024-05-28 11:15:30 +0700

    Implement the story list page view

--- app/src/main/java/com/arvl/fasimagiland/GettingStartedActivity.kt
+++ app/src/main/java/com/arvl/fasimagiland/GettingStartedActivity.kt
@@ -3,20 +3,24 @@ package com.arvl.fasimagiland
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arvl.fasimagiland.databinding.ActivityGettingStartedBinding
 
 class GettingStartedActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var binding: ActivityGettingStartedBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_getting_started)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityGettingStartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -24,8 +28,7 @@ class GettingStartedActivity : AppCompatActivity() {
 
         sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE)
 
-        val btnGetStarted = findViewById<Button>(R.id.btn_get_started)
-        btnGetStarted.setOnClickListener {
+        binding.btnGetStarted.setOnClickListener {
             val editor = sharedPreferences.edit()
             editor.putBoolean("isFirstRun", false)
             editor.apply()
@@ -36,4 +39,3 @@ class GettingStartedActivity : AppCompatActivity() {
         }
     }
 }
-
--- app/src/main/java/com/arvl/fasimagiland/SplashActivity.kt
+++ app/src/main/java/com/arvl/fasimagiland/SplashActivity.kt
@@ -1,19 +1,25 @@
 package com.arvl.fasimagiland
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.arvl.fasimagiland.databinding.ActivitySplashBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE)
         val isFirstRun = sharedPreferences.getBoolean("isFirstRun", true)
@@ -27,7 +33,6 @@ class SplashActivity : AppCompatActivity() {
                 startActivity(intent)
             }
             finish()
-        }, 2000) // Splash screen delay for 2 seconds
+        }, 3000)
     }
 }
-
